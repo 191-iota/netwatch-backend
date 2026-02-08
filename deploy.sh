e1ce336d658de6bd5fb4dd8a588028d456d81ad6
191-iota <101976629+191-iota@users.noreply.github.com>
2026-02-08 09:14:24 +0100

    automate deployment more

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,7 @@ source .env
 cross build --release --target aarch64-unknown-linux-gnu
 scp target/aarch64-unknown-linux-gnu/release/netwatch ${PI_USER}@${PI_HOST}:${PI_PATH}/
 ssh ${PI_USER}@${PI_HOST} "sudo setcap cap_net_raw=eip ${PI_PATH}/netwatch"
+scp deploy/netwatch.service $PI_USER@$PI_HOST:/tmp/
+ssh $PI_USER@$PI_HOST "sudo mv /tmp/netwatch.service /etc/systemd/system/ && sudo systemctl daemon-reload"
 echo "deployed. run: ssh ${PI_USER}@${PI_HOST} '${PI_PATH}/netwatch'"
+ssh $PI_USER@$PI_HOST "sudo systemctl restart netwatch"
